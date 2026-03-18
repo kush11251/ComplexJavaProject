c9fe89988242a3d53d6ce4b837a6a354bbe59a89
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 17:04:09 +0530

    feat: integrate src/main/java/com/company/app/Config.java

---
+++ src/main/java/com/company/app/Config.java
@@ -0,0 +1,5 @@
+public class Config {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "password";
+}
