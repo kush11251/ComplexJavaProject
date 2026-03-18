9aba97600200a4a674f0f7a415cf7fa205f55c11
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 17:04:14 +0530

    feat: integrate src/main/java/com/company/app/services/impl/UserServiceImpl.java

---
+++ src/main/java/com/company/app/services/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+import com.company.app.models.User;
+import com.company.app.repositories.UserRepository;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.getUsers();
+    }
+}
